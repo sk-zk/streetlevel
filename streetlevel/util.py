87f683395be032d8d76f21efa43e8514ec950136
sk-zk <sk-zk@users.noreply.github.com>
2023-07-17 01:37:06 +0200

    [Street View] Add async function for downloading panos

--- streetlevel/util.py
+++ streetlevel/util.py
@@ -13,11 +13,11 @@ def try_get(accessor):
         return None
 
 
-async def download_files_async(urls: List[str]) -> List[bytes]:
-    async with aiohttp.ClientSession() as session:
-        tasks = [session.get(url) for url in urls]
-        responses = await asyncio.gather(*tasks)
-        data = []
-        for response in responses:
-            data.append(await response.read())
-        return data
+async def download_files_async(urls: List[str], session: aiohttp.ClientSession = None) -> List[bytes]:
+    session = session if session else aiohttp.ClientSession()
+    tasks = [session.get(url) for url in urls]
+    responses = await asyncio.gather(*tasks)
+    data = []
+    for response in responses:
+        data.append(await response.read())
+    return data
