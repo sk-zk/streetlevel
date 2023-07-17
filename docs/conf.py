71c270c4fca5cb4eddcc8cbad16ee239742a8b71
sk-zk <sk-zk@users.noreply.github.com>
2023-07-17 11:50:17 +0200

    Move documentation to Sphinx

---
+++ docs/conf.py
@@ -0,0 +1,36 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'streetlevel'
+copyright = '2023, skzk'
+author = 'skzk'
+release = '0.4'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_style = 'css/additional.css'
+
+
+autodoc_typehints = "description"
+add_module_names = False
+
+import sys
+sys.path.insert(0, "..")
---
+++ docs/index.rst
@@ -0,0 +1,23 @@
+.. streetlevel documentation master file, created by
+   sphinx-quickstart on Mon Jul 17 09:13:14 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to streetlevel's documentation!
+=======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+   
+   streetlevel.streetview
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
+
