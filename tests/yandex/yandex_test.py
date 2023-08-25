4ca523055a004a48b41fe20164fdb1b9ed53fd13
sk-zk <sk-zk@users.noreply.github.com>
2023-08-25 13:52:29 +0200

    [Yandex] Add test

---
+++ tests/yandex/yandex_test.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from pytest import approx
+import json
+
+from requests import Session
+
+from streetlevel import yandex
+
+
+def mocked_find_panorama_raw(lat: float, lon: float, session: Session = None) -> dict:
+    with open("yandex/data/find_object.json", "r") as f:
+        return json.load(f)
+
+
+yandex.api.find_panorama_raw = mocked_find_panorama_raw
+
+
+def test_find_panorama():
+    pano = yandex.find_panorama(53.917633, 27.548128)
+    assert pano.id == "1238072810_692204477_23_1688105969"
+    assert pano.lat == approx(53.917633, 0.001)
+    assert pano.lon == approx(27.548128, 0.001)
+    assert pano.date == datetime.utcfromtimestamp(1688105969)
+    assert pano.image_id == "WhpeuZGm2FIL"
+    assert pano.image_sizes[0].x == 17664
+    assert pano.image_sizes[0].y == 6145
