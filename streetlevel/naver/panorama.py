e1efb6a335d44ad8fc4c66f21fe6539279eb7b3f
sk-zk <sk-zk@users.noreply.github.com>
2023-10-01 17:34:42 +0200

    [Naver] Add permalink

--- streetlevel/naver/panorama.py
+++ streetlevel/naver/panorama.py
@@ -1,4 +1,6 @@
 from __future__ import annotations
+
+import math
 from dataclasses import dataclass
 from datetime import datetime
 from enum import IntEnum
@@ -82,6 +84,23 @@ class NaverPanorama:
     
     (Only available for car footage.)
     """
+
+    def permalink(self: NaverPanorama, heading: float = 0.0, pitch: float = 10.0, fov: float = 80.0,
+                  map_zoom: float = 17.0, radians: bool = False) -> str:
+        """
+        Creates a permalink to this panorama.
+
+        :param heading: *(optional)* Initial heading of the viewport. Defaults to 0°.
+        :param pitch: *(optional)* Initial pitch of the viewport. Defaults to 10°.
+        :param fov: *(optional)* Initial FOV of the viewport. Defaults to 80.
+        :param map_zoom: *(optional)* Initial zoom level of the map. Defaults to 17.
+        :param radians: *(optional)* Whether angles are in radians. Defaults to False.
+        :return: A Naver Map URL.
+        """
+        if radians:
+            heading = math.degrees(heading)
+            pitch = math.degrees(pitch)
+        return f"https://map.naver.com/p?c={map_zoom},0,0,0,adh&p={self.id},{heading},{pitch},{fov},Float"
 
     def __repr__(self):
         output = str(self)
