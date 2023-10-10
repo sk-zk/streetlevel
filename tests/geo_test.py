b9a72aaa4613e0892fcb7f6dee7096fab0a94d79
sk-zk <sk-zk@users.noreply.github.com>
2023-10-10 16:05:23 +0200

    Add tests (#2)

--- tests/geo_test.py
+++ tests/geo_test.py
@@ -1,3 +1,5 @@
+import math
+
 from pytest import approx
 from streetlevel import geo
 
@@ -13,3 +15,11 @@ def tile_coord_to_wgs84():
     actual = geo.tile_coord_to_wgs84(69172, 42368, 17)
     assert expected[0] == approx(actual[0], 0.00001)
     assert expected[1] == approx(actual[1], 0.00001)
+
+
+def test_get_bearing():
+    lat1, lon1 = 56.4022257183309, 10.419846839556701
+    lat2, lon2 = 56.493210323925524, 10.6829783303312
+    expected = 1.0107660582679734
+    actual = geo.get_bearing(lat1, lon1, lat2, lon2)
+    assert actual == approx(expected)
