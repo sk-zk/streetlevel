8acddd0aec3839f2148214d8fd68e76be0e4baf5
Megan Leet <megan.leet@iinet.net.au>
2023-10-23 02:17:46 +1100

    Add test for missing building level name

--- tests/streetview/streetview_test.py
+++ tests/streetview/streetview_test.py
@@ -110,3 +110,17 @@ def test_places():
     assert pano.places[2].marker_yaw == approx(1.6297568407941114)
     assert pano.places[2].marker_pitch == approx(-0.00047187885564841503)
     assert pano.places[2].marker_distance == approx(29.35295486450195)
+
+
+def test_missing_level_name():
+    def mocked_find_panorama_by_id_raw(panoid, download_depth=False, locale="en", session=None):
+        with open("streetview/data/missing_level_name.json", "r") as f:
+            return json.load(f)
+
+    streetview.api.find_panorama_by_id_raw = mocked_find_panorama_by_id_raw
+
+    pano = streetview.find_panorama_by_id("4pRBISc-WOW0Qw8kB8mC3Q")
+    assert pano.building_level.level == 0
+    assert pano.building_level.name is None
+    assert pano.building_level.short_name is None
+    
