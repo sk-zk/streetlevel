24f33d922a974a5699b0e0460a8cfafd30cb1f51
sk-zk <sk-zk@users.noreply.github.com>
2024-12-14 18:21:05 +0100

    [Yandex] Fix metadata requests getting 429 response

--- streetlevel/yandex/api.py
+++ streetlevel/yandex/api.py
@@ -2,6 +2,9 @@ from aiohttp import ClientSession
 from requests import Session
 from ..util import get_json, get_json_async
 
+_user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) " \
+              "Chrome/131.0.0.0 Safari/537.36"
+
 
 def build_find_panorama_request_url(lat: float, lon: float) -> str:
     return f"https://api-maps.yandex.ru/services/panoramas/1.x/?l=stv&lang=en_US" \
@@ -14,16 +17,20 @@ def build_find_panorama_by_id_request_url(panoid: str) -> str:
 
 
 def find_panorama(lat: float, lon: float, session: Session = None) -> dict:
-    return get_json(build_find_panorama_request_url(lat, lon), session=session)
+    return get_json(build_find_panorama_request_url(lat, lon), session=session,
+                    headers={"User-Agent": _user_agent})
 
 
 async def find_panorama_async(lat: float, lon: float, session: ClientSession) -> dict:
-    return await get_json_async(build_find_panorama_request_url(lat, lon), session)
+    return await get_json_async(build_find_panorama_request_url(lat, lon), session,
+                                headers={"User-Agent": _user_agent})
 
 
 def find_panorama_by_id(panoid: str, session: Session = None) -> dict:
-    return get_json(build_find_panorama_by_id_request_url(panoid), session=session)
+    return get_json(build_find_panorama_by_id_request_url(panoid), session=session,
+                    headers={"User-Agent": _user_agent})
 
 
 async def find_panorama_by_id_async(panoid: str, session: ClientSession) -> dict:
-    return await get_json_async(build_find_panorama_by_id_request_url(panoid), session)
+    return await get_json_async(build_find_panorama_by_id_request_url(panoid), session,
+                                headers={"User-Agent": _user_agent})
