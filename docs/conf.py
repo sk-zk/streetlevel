d9d9d1e331571bc6b3d2e390b3d5186432e1f84c
sk-zk <sk-zk@users.noreply.github.com>
2025-07-12 13:47:09 +0200

    Increase version

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 project = 'streetlevel'
 copyright = '2025, skzk'
 author = 'skzk'
-release = '0.12.3'
+release = '0.12.4'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
